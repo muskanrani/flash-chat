a82786ceb7ca041b9d7e0f6073da4a92a5c8cde6
muskanrani <muskanrani@users.noreply.github.com>
2020-12-01 11:30:17 +0530

    Refactoring code

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -2,7 +2,7 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
-
+import 'package:flash_chat/components/RoundedButton.dart';
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'welcome_screen';
   @override
@@ -63,49 +63,21 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
             SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to login screen.
-
-                      Navigator.pushNamed(context, LoginScreen.id);
-
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
+            RoundedButton(
+                colour: Colors.lightBlueAccent,
+                title: 'Login',
+                onPressed:(){
+                  Navigator.pushNamed(context, LoginScreen.id);
+                }
                 ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to registration screen.
-                    Navigator.pushNamed(context, RegistrationScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
-              ),
-            ),
+            RoundedButton(onPressed: (){
+              Navigator.pushNamed(context, RegistrationScreen.id);},
+              title: 'Register',
+              colour: Colors.blueAccent,),
           ],
         ),
       ),
     );
   }
 }
+
