dacc418e84b35614928900539a71c874f8754036
muskanrani <muskanrani@users.noreply.github.com>
2021-01-29 00:05:27 +0530

    Registering user successfully

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flash_chat/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/components/RoundedButton.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'chat_screen.dart';
 class RegistrationScreen extends StatefulWidget {
   static const String id = 'registration_screen';
   @override
@@ -8,6 +10,7 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _auth = FirebaseAuth.instance;
   String email, password;
   @override
   Widget build(BuildContext context) {
@@ -57,9 +60,17 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             RoundedButton(
                 colour: Colors.blueAccent,
                 title: 'Register',
-                onPressed:(){
-                  print(email);
-                  print(password);
+                onPressed:() async{
+                  try {
+                    final newUser = await _auth.createUserWithEmailAndPassword(
+                        email: email, password: password);
+                    if(newUser != null){
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+                  }
+                  catch(e){
+                    print(e);
+                  }
                 }
             ),
 
